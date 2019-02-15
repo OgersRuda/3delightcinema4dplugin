cff484be8d6301bfbe4fdb352bf3c49f73aec6df
Ogers <ogers.ruda@gmail.com>
2019-02-15 11:29:53 +0100

    Work in progress for Render Settings.

--- 3DLfC4D lights/source/IDs.h
+++
@@ -1,9 +0,0 @@
-#ifndef IDS_H
-#define IDS_H
-
-//IDs obtained from plugincafe.com
-#define ID_LIGHTCARD 1041233 
-#define ID_ENVIRONMENTLIGHT 1041234 
-#define ID_POINTLIGHT 10412398 //Get from café 
-
-#endif
--- 3DLfC4D/source/DL_Render.h
+++
@@ -1,9 +0,0 @@
-#ifndef NORRSKEN_H
-#define NORRSKEN_H
-
-#include "c4d.h"
-#include "DL_API.h"
-
-bool DL_RenderFrame(BaseDocument* doc, long frame, RENDER_MODE mode = PREVIEW_RENDER, bool progressive = false);
-
-#endif
